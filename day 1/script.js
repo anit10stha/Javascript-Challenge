66c8801705727b7bac751369e0789a2e8c51163b
Anit Shrestha <anitshrestha10@gmail.com>
2024-09-19 13:10:29 +0545

    initial commit

---
+++ day 1/script.js
@@ -0,0 +1,7 @@
+document.getElementById('on').addEventListener('click', function(){
+    document.getElementById('circle').style.backgroundColor = 'green'
+})
+
+document.getElementById('off').addEventListener('click', function(){
+    document.getElementById('circle').style.backgroundColor = 'red'
+})
