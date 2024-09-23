c212feece8ccb4edaaf951de2de7aa01665bdb6c
Anit Shrestha <anitshrestha10@gmail.com>
2024-09-23 20:31:13 +0545

    files added

---
+++ GSAP library/script.js
@@ -0,0 +1,20 @@
+gsap.to("#box1", {
+    x: 900,
+    y: 100,
+    duration: 3,
+    delay: 1.5,
+    rotate: 360,
+    borderRadius: "50%",
+    repeat: -1
+});
+
+gsap.to("#box2", {
+    x: -900,
+    y: 100,
+    duration: 3,
+    delay: 1.5,
+    rotate: 360,
+    borderRadius: "50%",
+    repeat: -1
+});
+
